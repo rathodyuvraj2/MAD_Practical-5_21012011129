ceb633a900c51a4c32881a1d8a109ff77935c8a9
student <student@gnu.ac.in>
2023-08-18 08:57:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_practical_5_21012011129/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.mad_practical_5_21012011129
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.AlarmClock
+import android.provider.CallLog
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        val buttonBrowse : Button = findViewById(R.id.button_browse)
+//        val editUrl : EditText = findViewById(R.id.edittexturl)
+
+//        buttonBrowse.setOnClickListener { openBrowser(editUrl.text.toString()) }
+    }
+
+    fun openBrowser(url:String){
+        Intent(Intent.ACTION_VIEW, Uri.parse(url)).also { startActivity(it) }
+
+    }
+
+    fun openCall(number:String){
+        Intent(Intent.ACTION_DIAL).setData(Uri.parse("tel: $number")).also { startActivity(it) }
+
+    }
+
+    fun openCallLog(){
+        Intent(Intent.ACTION_VIEW).setType(CallLog.Calls.CONTENT_TYPE).also { startActivity(it) }
+
+    }
+
+    fun openGallery(){
+        Intent(Intent.ACTION_VIEW).setType("image/*").also { startActivity(it) }
+
+    }
+
+    fun openCamera(){
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { startActivity(it) }
+
+    }
+
+    fun openAlarm(){
+        Intent(AlarmClock.ACTION_SHOW_ALARMS).also { startActivity(it) }
+
+    }
+}
